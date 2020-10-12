a93e5bd781ffeb017a5f9497392c648a49754be3
Johnwclemens <jwclemens@yahoo.com>
2020-10-11 20:47:53 -0500

    _initImgSprts(opq=1), _initSciSprts(opq=1) opacity const or linear

--- testGui.py
+++ testGui.py
@@ -8,60 +8,73 @@ def fri(f): return int(math.floor(f+0.5))
 
 class TestGui(pyglet.window.Window):
     def __init__(self):
-        self.ww, self.wh = 1000, 600
+        self._ww, self._wh = 1000, 600
+        self.ww, self.wh = self._ww, self._wh
         self.nc, self.nr = 20, 12
         self.w,  self.h  = self.ww/self.nc, self.wh/self.nr
         super(TestGui, self).__init__(width=self.ww, height=self.wh, resizable=True, visible=False)
         self.batch = pyglet.graphics.Batch()
-        self._ww, self._wh = self.ww, self.wh
-        self.COLORS=[(127, 127, 127, 195), (66, 60, 144, 195)]
-        self.useSprites, self.useCBS = True, True
-        if self.useCBS:     self._initCBS()
-        if self.useSprites: self._initSprites()
+        self.COLORS=[(227, 147, 127, 255), (66, 60, 144, 255)]
+        self.useSciSprts, self.useImgSprts = True, True
+        if self.useSciSprts: self._initSciSprts(opq=False)
+        if self.useImgSprts: self._initImgSprts(opq=False)
         self.set_visible(True)
 
-    def _initSprites(self):
+    def _initImgSprts(self, opq=True):
         w, h = self.w, self.h
         ww, wh = self.ww, self.wh
-        self.sprites, imgs, i = [], [], 0
-        s = ['C:/Python36/my/tabs/apple_raw.png', 'C:/Python36/my/tabs/pyglet.png']
-        self.SIM = []
+        self.imgSprts, imgs, i = [], [], 0
+        s = ['C:/Python36/my/tabs/apple_raw.png', 'C:/Python36/my/tabs/pyglet.png', 'C:/Python36/my/tabs/asteroid.png']
+        self.imgSprtScales = []
         decoder = pyglet.image.codecs.png.PNGImageDecoder()
-        print('_initSprites(BGN) ww={} wh={} nc={} nr={} w={:5.1f} h={:5.1f}'.format(self.ww, self.wh, self.nc, self.nr, w, h), file=DBG_FILE)
-        for i, imgFileName in enumerate(s):
-            tmp = pyglet.image.load(imgFileName, decoder=decoder)
-            imgs.append(tmp)
-            self.SIM.append([w/tmp.width, h/tmp.height])
-            print('_initSprites() [{}] w={:5.1f} h={:5.1f} f={}'.format(i, w, h, imgFileName), file=DBG_FILE)
+        print('_initImgSprts(BGN) ww={} wh={} nc={} nr={} w={:5.1f} h={:5.1f}'.format(self.ww, self.wh, self.nc, self.nr, w, h), file=DBG_FILE)
+        for i, imageFileName in enumerate(s):
+            img = pyglet.image.load(imageFileName, decoder=decoder)
+            imgs.append(img)
+            self.imgSprtScales.append([w/img.width, h/img.height])
+            print('_initImgSprts()      [{}] w={:5.1f} h={:5.1f} f={}'.format(i, w, h, imageFileName), file=DBG_FILE)
         for r in range(self.nr):
-            tmp = []
+            imgSprts = []
+            if opq: opacity = 255
+            else:   opacity = fri(255*r/(self.nr-1))
+#            else:   opacity = fri(128*r/self.nr)+127
             for c in range(self.nc):
                 x, y = c*w, self.wh-h-r*h
-                sprite = pyglet.sprite.Sprite(imgs[(c+r)%(i+1)], x=x, y=y, batch=self.batch, subpixel=False)
-                sw, sh = sprite.width, sprite.height
-                sprite.scale_x, sprite.scale_y = w/sw, h/sh
-                p, q = sprite.scale_x, sprite.scale_y
-                tmp.append(sprite)
-                if c == self.nc-1: print('_initSprites()     [{:2}][{:2}] x={:6.1f} y={:6.1f} sw={:3} sh={:3} p={:5.3f} q={:5.3f} w={:5.1f} h={:5.1f} ww={} wh={}'.format(r, c, x, y, sw, sh, p, q, w, h, ww, wh), file=DBG_FILE)
-            self.sprites.append(tmp)
+                imgSprt = pyglet.sprite.Sprite(imgs[(c+r)%(i+1)], x=x, y=y, batch=self.batch, subpixel=False)
+                imgSprt.opacity = opacity
+                sw, sh = imgSprt.width, imgSprt.height
+                imgSprt.scale_x, imgSprt.scale_y = w/sw, h/sh
+                p, q = imgSprt.scale_x, imgSprt.scale_y
+                imgSprts.append(imgSprt)
+                if c == self.nc-1: print('_initImgSprts()     [{:2}][{:2}] x={:6.1f} y={:6.1f} sw={:3} sh={:3} p={:5.3f} q={:5.3f} w={:5.1f} h={:5.1f} ww={} wh={} opacity={}'.format(r, c, x, y, sw, sh, p, q, w, h, ww, wh, opacity), file=DBG_FILE)
+            self.imgSprts.append(imgSprts)
 
-    def _initCBS(self):
+    def _initSciSprts(self, opq=True):
         w, h = self.w, self.h
         ww, wh = self.ww, self.wh
-        self.CBS, cbs = [], None
-        print('_initCBS(BGN)     ww={} wh={} nc={} nr={} w={:5.1f} h={:5.1f}'.format(self.ww, self.wh, self.nc, self.nr, w, h), file=DBG_FILE)
+        self.sciSprts = []
+        print('_initSciSprts(BGN) ww={} wh={} nc={} nr={} w={:5.1f} h={:5.1f}'.format(self.ww, self.wh, self.nc, self.nr, w, h), file=DBG_FILE)
         for r in range(self.nr):
-            tmp=[]
+            sciSprts=[]
             for c in range(self.nc):
                 x, y = c*w, self.wh-h-r*h
-                cbp = pyglet.image.SolidColorImagePattern(self.COLORS[(c+r)%2])
-                cbi = cbp.create_image(width=fri(w), height=fri(h))
-                cbs = pyglet.sprite.Sprite(img=cbi, x=x, y=y, batch=self.batch, subpixel=False)
-                sw, sh = cbs.width, cbs.height
-                p, q = cbs.scale_x, cbs.scale_y
-                tmp.append(cbs)
-                if c == self.nc-1: print('  _initCBS()       [{:2}][{:2}] x={:6.1f} y={:6.1f} sw={:3} sh={:3} p={:5.3f} q={:5.3f} w={:5.1f} h={:5.1f} ww={} wh={}'.format(r, c, x, y, sw, sh, p, q, w, h, ww, wh), file=DBG_FILE)
-            self.CBS.append(tmp)
+#                color = list(self.COLORS[(c+r)%2])
+#                if opq: opacity = 255
+#                else:   opacity = fri(255*c/self.nc)
+#                else:   color[3] = fri(128*c/self.nc)+127
+#                color[3] = opacity
+#                scip = pyglet.image.SolidColorImagePattern(tuple(color))
+                scip = pyglet.image.SolidColorImagePattern(self.COLORS[(c+r)%2])
+                sci = scip.create_image(width=fri(w), height=fri(h))
+                sciSprt = pyglet.sprite.Sprite(img=sci, x=x, y=y, batch=self.batch, subpixel=False)
+                if opq: opacity = 255
+                else:   opacity = fri(255*c/(self.nc-1))
+                sciSprt.opacity = opacity
+                sw, sh = sciSprt.width, sciSprt.height
+                p, q = sciSprt.scale_x, sciSprt.scale_y
+                sciSprts.append(sciSprt)
+                if r == self.nr-1: print('_initSciSprts()     [{:2}][{:2}] x={:6.1f} y={:6.1f} sw={:3} sh={:3} p={:5.3f} q={:5.3f} w={:5.1f} h={:5.1f} ww={} wh={} opacity={}'.format(r, c, x, y, sw, sh, p, q, w, h, ww, wh, opacity), file=DBG_FILE)
+            self.sciSprts.append(sciSprts)
 
     def on_resize(self, width, height):
         _ww, _wh = self._ww, self._wh
@@ -70,21 +83,20 @@ class TestGui(pyglet.window.Window):
         self.w, self.h = self.ww/self.nc, self.wh/self.nr
         ww, wh = self.ww, self.wh
         w, h = self.w, self.h
-#        print('on_resize(BGN) _p={:5.3f} _q={:5.3f} _ww={} _wh={} nc={} nr={} ww={} wh={} w={:5.1f} h={:5.1f}'.format(_p, _q, _ww, _wh, self.nc, self.nr, ww, wh, w, h), file=DBG_FILE)
-        print('on_resize(BGN) _ww={} _wh={} nc={} nr={} ww={} wh={} w={:5.1f} h={:5.1f}'.format(_ww, _wh, self.nc, self.nr, ww, wh, w, h), file=DBG_FILE)
+        print('on_resize(BGN)     _ww={} _wh={} nc={} nr={} ww={} wh={} w={:5.1f} h={:5.1f}'.format(_ww, _wh, self.nc, self.nr, ww, wh, w, h), file=DBG_FILE)
         for r in range(self.nr):
             for c in range(self.nc):
                 x, y = c*w, self.wh-h-r*h
-                if self.useCBS:
-                    self.CBS[r][c].update(x=x, y=y, scale_x=width/_ww, scale_y=height/_wh)
-                    sw, sh = self.CBS[r][c].width, self.CBS[r][c].height
-                    p, q = self.CBS[r][c].scale_x, self.CBS[r][c].scale_y
-                    if c == self.nc-1: print('on_resize(CBS)     [{:2}][{:2}] x={:6.1f} y={:6.1f} sw={:3} sh={:3} p={:5.3f} q={:5.3f} w={:5.1f} h={:5.1f} ww={:4} wh={:4}'.format(r, c, x, y, sw, sh, p, q, w, h, ww, wh), file=DBG_FILE)
-                if self.useSprites:
-                    self.sprites[r][c].update(x=x, y=y, scale_x=self.SIM[(r+c)%len(self.SIM)][0]*ww/_ww, scale_y=self.SIM[(r+c)%len(self.SIM)][0]*wh/_wh)
-                    sw, sh = self.sprites[r][c].width, self.sprites[r][c].height
-                    p, q = self.sprites[r][c].scale_x, self.sprites[r][c].scale_y
-                    if c == self.nc-1: print('on_resize(sprites) [{:2}][{:2}] x={:6.1f} y={:6.1f} sw={:3} sh={:3} p={:5.3f} q={:5.3f} w={:5.1f} h={:5.1f} ww={:4} wh={:4}'.format(r, c, x, y, sw, sh, p, q, w, h, ww, wh), file=DBG_FILE)
+                if self.useSciSprts:
+                    self.sciSprts[r][c].update(x=x, y=y, scale_x=width/_ww, scale_y=height/_wh)
+                    sw, sh = self.sciSprts[r][c].width, self.sciSprts[r][c].height
+                    p, q = self.sciSprts[r][c].scale_x, self.sciSprts[r][c].scale_y
+                    if c == self.nc-1: print('on_resize(sciSprts) [{:2}][{:2}] x={:6.1f} y={:6.1f} sw={:3} sh={:3} p={:5.3f} q={:5.3f} w={:5.1f} h={:5.1f} ww={:4} wh={:4}'.format(r, c, x, y, sw, sh, p, q, w, h, ww, wh), file=DBG_FILE)
+                if self.useImgSprts:
+                    self.imgSprts[r][c].update(x=x, y=y, scale_x=self.imgSprtScales[(r+c)%len(self.imgSprtScales)][0]*ww/_ww, scale_y=self.imgSprtScales[(r+c)%len(self.imgSprtScales)][0]*wh/_wh)
+                    sw, sh = self.imgSprts[r][c].width, self.imgSprts[r][c].height
+                    p, q = self.imgSprts[r][c].scale_x, self.imgSprts[r][c].scale_y
+                    if c == self.nc-1: print('on_resize(imgSprts) [{:2}][{:2}] x={:6.1f} y={:6.1f} sw={:3} sh={:3} p={:5.3f} q={:5.3f} w={:5.1f} h={:5.1f} ww={:4} wh={:4}'.format(r, c, x, y, sw, sh, p, q, w, h, ww, wh), file=DBG_FILE)
 
     def on_draw(self):
         self.clear()
