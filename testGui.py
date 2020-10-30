c2cd1162b1b203fa4039ae50eb6ee4c5ff619fc5
Johnwclemens <jwclemens@yahoo.com>
2020-10-30 01:51:56 -0500

    testGui.py getMeshColor() refactoring init & resize

--- testGui.py
+++ testGui.py
@@ -7,7 +7,7 @@ class TestGui(pyglet.window.Window):
     def __init__(self):
         self._ww, self._wh = 1000, 600
         self.ww, self.wh = self._ww, self._wh
-        self.nc, self.nr = 19, 7# 17, 11
+        self.nc, self.nr = 21, 13# 17, 11
         self.w,  self.h  = self.ww/self.nc, self.wh/self.nr
         super(TestGui, self).__init__(width=self.ww, height=self.wh, resizable=True, visible=False)
         pyglet.resource.path = ['res', 'res/images']
@@ -34,76 +34,56 @@ class TestGui(pyglet.window.Window):
 
     def _initGrid(self, dbg=1):
         self.lineGroup = self._initGroup(2)
-        self.MIN, self.NOM, self.MAX = 0, 1, 2
-        self.MESH      = [(255, 0, 0), (0, 255, 0), (255, 255, 255)]
         mesh, color    = [1, 5, 10], self.COLORS[0]
         _ww, _wh, ww, wh, w, h, nc, nr = self.getGeom()
         self.dumpGeom('_initGrid(BGN)')
         self.clines, self.rlines = [], []
         if nc % 2 == 0:
-            p = fri(nc/2) % mesh[self.MAX]
+            p = fri(nc/2) % mesh[len(mesh)]
             if dbg: print('_initGrid() nc={}=Even p={}'.format(nc, p), file=DBG_FILE)
             for c in range(nc+1):
-                x1, y1 = c*w, 0
-                x2, y2 = c*w, nr*h
-                if   (c-p) % mesh[self.MAX] == 0: color = self.MESH[self.MAX]; #print('(i-p)%{}={}'.format(mesh[self.MAX], (i-p) % mesh[self.MAX]), file=DBG_FILE)
-                elif (c-p) % mesh[self.NOM] == 0: color = self.MESH[self.NOM]; #print('(i-p)%{}={}'.format(mesh[self.NOM], (i-p) % mesh[self.NOM]), file=DBG_FILE)
-                elif (c-p) % mesh[self.MIN] == 0: color = self.MESH[self.MIN]; #print('(i-p)%{}={}'.format(mesh[self.MIN], (i-p) % mesh[self.MIN]), file=DBG_FILE)
-                self.clines.append(pyglet.shapes.Line(x=x1, y=y1, x2=x2, y2=y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
-                if dbg: print('c=[{:3}] x1={:6.1f} y1={:6.1f} x2={:6.1f} y2={:6.1f}'.format(c, x1, y1, x2, y2), file=DBG_FILE)
+                x1, x2, y1, y2 = c*w, c*w, 0, nr*h
+                color = self.getMeshColor(c, p, mesh)
+                self.clines.append(pyglet.shapes.Line(x1, y1, x2, y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
+                if dbg: print('c=[{:3}] x1={:7.2f} y1={:7.2f} x2={:7.2f} y2={:7.2f}'.format(c, x1, y1, x2, y2), file=DBG_FILE)
         else:
             p = fri(nc/2-1)
             if dbg: print('_initGrid() nc={}=Odd p={}'.format(nc, p), file=DBG_FILE)
             for c in range(fri(nc/2)):
-                x1, y1 = c*w, 0
-                x2, y2 = c*w, nr*h
-                if   (c-p) % mesh[self.MAX] == 0: color = self.MESH[self.MAX]; #print('(i-p)%{}={}'.format(mesh[self.MAX], (i-p) % mesh[self.MAX]), file=DBG_FILE)
-                elif (c-p) % mesh[self.NOM] == 0: color = self.MESH[self.NOM]; #print('(i-p)%{}={}'.format(mesh[self.NOM], (i-p) % mesh[self.NOM]), file=DBG_FILE)
-                elif (c-p) % mesh[self.MIN] == 0: color = self.MESH[self.MIN]; #print('(i-p)%{}={}'.format(mesh[self.MIN], (i-p) % mesh[self.MIN]), file=DBG_FILE)
-                self.clines.append(pyglet.shapes.Line(x=x1, y=y1, x2=x2, y2=y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
-                if dbg: print('c=[{:3}] x1={:6.1f} y1={:6.1f} x2={:6.1f} y2={:6.1f}'.format(c, x1, y1, x2, y2), file=DBG_FILE)
+                x1, x2, y1, y2 = c*w, c*w, 0, nr*h
+                color = self.getMeshColor(c, p, mesh)
+                self.clines.append(pyglet.shapes.Line(x1, y1, x2, y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
+                if dbg: print('c=[{:3}] x1={:7.2f} y1={:7.2f} x2={:7.2f} y2={:7.2f}'.format(c, x1, y1, x2, y2), file=DBG_FILE)
             p = fri(nc/2)
             if dbg: print('_initGrid() nc={}=Odd p={}'.format(nc, p), file=DBG_FILE)
             for c in range(fri(nc/2), nc+1):
-                x1, y1 = c*w, 0
-                x2, y2 = c*w, nr*h
-                if   (c-p) % mesh[self.MAX] == 0: color = self.MESH[self.MAX]; #print('(i-p)%{}={}'.format(mesh[self.MAX], (i-p) % mesh[self.MAX]), file=DBG_FILE)
-                elif (c-p) % mesh[self.NOM] == 0: color = self.MESH[self.NOM]; #print('(i-p)%{}={}'.format(mesh[self.NOM], (i-p) % mesh[self.NOM]), file=DBG_FILE)
-                elif (c-p) % mesh[self.MIN] == 0: color = self.MESH[self.MIN]; #print('(i-p)%{}={}'.format(mesh[self.MIN], (i-p) % mesh[self.MIN]), file=DBG_FILE)
-                self.clines.append(pyglet.shapes.Line(x=x1, y=y1, x2=x2, y2=y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
-                if dbg: print('c=[{:3}] x1={:6.1f} y1={:6.1f} x2={:6.1f} y2={:6.1f}'.format(c, x1, y1, x2, y2), file=DBG_FILE)
+                x1, x2, y1, y2 = c*w, c*w, 0, nr*h
+                color = self.getMeshColor(c, p, mesh)
+                self.clines.append(pyglet.shapes.Line(x1, y1, x2, y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
+                if dbg: print('c=[{:3}] x1={:7.2f} y1={:7.2f} x2={:7.2f} y2={:7.2f}'.format(c, x1, y1, x2, y2), file=DBG_FILE)
         if nr % 2 == 0:
-            q = fri(nr/2) % mesh[self.MAX]
+            q = fri(nr/2) % mesh[len(mesh)]
             if dbg: print('_initGrid() nr={}=Even q={}'.format(nr, q), file=DBG_FILE)
             for r in range(nr+1):
-                x1, y1 = 0,    r*h
-                x2, y2 = nc*w, r*h
-                if   (r-q) % mesh[self.MAX] == 0: color = self.MESH[self.MAX]; #print('(j-q)%{}={}'.format(mesh[self.MAX], (j-q) % mesh[self.MAX]), file=DBG_FILE)
-                elif (r-q) % mesh[self.NOM] == 0: color = self.MESH[self.NOM]; #print('(j-q)%{}={}'.format(mesh[self.NOM], (j-q) % mesh[self.NOM]), file=DBG_FILE)
-                elif (r-q) % mesh[self.MIN] == 0: color = self.MESH[self.MIN]; #print('(j-q)%{}={}'.format(mesh[self.MIN], (j-q) % mesh[self.MIN]), file=DBG_FILE)
-                self.rlines.append(pyglet.shapes.Line(x=x1, y=y1, x2=x2, y2=y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
-                if dbg: print('r=[{:3}] x1={:6.1f} y1={:6.1f} x2={:6.1f} y2={:6.1f}'.format(r, x1, y1, x2, y2), file=DBG_FILE)
+                x1, x2, y1, y2 = 0, nc*w, r*h, r*h
+                color = self.getMeshColor(r, q, mesh)
+                self.rlines.append(pyglet.shapes.Line(x1, y1, x2, y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
+                if dbg: print('r=[{:3}] x1={:7.2f} y1={:7.2f} x2={:7.2f} y2={:7.2f}'.format(r, x1, y1, x2, y2), file=DBG_FILE)
         else:
             q = fri(nr/2-1)
             if dbg: print('_initGrid() nr={}=Odd q={}'.format(nr, q), file=DBG_FILE)
             for r in range(fri(nr/2)):
-                x1, y1 = 0,    r*h
-                x2, y2 = nc*w, r*h
-                if   (r-q) % mesh[self.MAX] == 0: color = self.MESH[self.MAX]; #print('(j-q)%{}={}'.format(mesh[self.MAX], (j-q) % mesh[self.MAX]), file=DBG_FILE)
-                elif (r-q) % mesh[self.NOM] == 0: color = self.MESH[self.NOM]; #print('(j-q)%{}={}'.format(mesh[self.NOM], (j-q) % mesh[self.NOM]), file=DBG_FILE)
-                elif (r-q) % mesh[self.MIN] == 0: color = self.MESH[self.MIN]; #print('(j-q)%{}={}'.format(mesh[self.MIN], (j-q) % mesh[self.MIN]), file=DBG_FILE)
-                self.rlines.append(pyglet.shapes.Line(x=x1, y=y1, x2=x2, y2=y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
-                if dbg: print('r=[{:3}] x1={:6.1f} y1={:6.1f} x2={:6.1f} y2={:6.1f}'.format(r, x1, y1, x2, y2), file=DBG_FILE)
+                x1, x2, y1, y2 = 0, nc*w, r*h, r*h
+                color = self.getMeshColor(r, q, mesh)
+                self.rlines.append(pyglet.shapes.Line(x1, y1, x2, y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
+                if dbg: print('r=[{:3}] x1={:7.2f} y1={:7.2f} x2={:7.2f} y2={:7.2f}'.format(r, x1, y1, x2, y2), file=DBG_FILE)
             q = fri(nr/2)
             if dbg: print('_initGrid() nr={}=odd q={}'.format(nr, q), file=DBG_FILE)
             for r in range(fri(nr/2), nr+1):
-                x1, y1 = 0,    r*h
-                x2, y2 = nc*w, r*h
-                if   (r-q) % mesh[self.MAX] == 0: color = self.MESH[self.MAX]; #print('(j-q)%{}={}'.format(mesh[self.MAX], (j-q) % mesh[self.MAX]), file=DBG_FILE)
-                elif (r-q) % mesh[self.NOM] == 0: color = self.MESH[self.NOM]; #print('(j-q)%{}={}'.format(mesh[self.NOM], (j-q) % mesh[self.NOM]), file=DBG_FILE)
-                elif (r-q) % mesh[self.MIN] == 0: color = self.MESH[self.MIN]; #print('(j-q)%{}={}'.format(mesh[self.MIN], (j-q) % mesh[self.MIN]), file=DBG_FILE)
-                self.rlines.append(pyglet.shapes.Line(x=x1, y=y1, x2=x2, y2=y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
-                if dbg: print('r=[{:3}] x1={:6.1f} y1={:6.1f} x2={:6.1f} y2={:6.1f}'.format(r, x1, y1, x2, y2), file=DBG_FILE)
+                x1, x2, y1, y2 = 0, nc*w, r*h, r*h
+                color = self.getMeshColor(r, q, mesh)
+                self.rlines.append(pyglet.shapes.Line(x1, y1, x2, y2, width=1, color=color, batch=self.batch, group=self.lineGroup))
+                if dbg: print('r=[{:3}] x1={:7.2f} y1={:7.2f} x2={:7.2f} y2={:7.2f}'.format(r, x1, y1, x2, y2), file=DBG_FILE)
 
     def _initImgSprts(self):
         self.imgGroup = self._initGroup(1)
@@ -118,7 +98,6 @@ class TestGui(pyglet.window.Window):
             iss.append([w/img.width, h/img.height])
             img.anchor_x, img.anchor_y = fri(img.width/2), fri(img.height/2)
             print('_initImgSprts() i=[{:2}] iax={:4} iay={:4} w={:8.2f} h={:8.2f} imx={:5.3f} imy={:5.3f} {:6.1f} {:6.1f} f={}'.format(i, img.anchor_x, img.anchor_y, img.width, img.height, iss[i][0], iss[i][1], img.width*iss[i][0], img.height*iss[i][1], imageFileName), file=DBG_FILE)
-#            print('_initImgSprts() i=[{:2}] iax={:4} iay={:4} w={:4} h={:4} imx={:5.3f} imy={:5.3f} {:6.1f} {:6.1f} f={}'.format(i, img.anchor_x, img.anchor_y, img.width, img.height, iss[i][0], iss[i][1], img.width*iss[i][0], img.height*iss[i][1], imageFileName), file=DBG_FILE)
         self.dumpSprite(None)
         for r in range(nr):
             imgSprts = []
@@ -147,11 +126,19 @@ class TestGui(pyglet.window.Window):
     def createSprite(self, reason, img, grp, c, r, w, h, i, ni):
         img.anchor_x, img.anchor_y = fri(w/2), fri(h/2)
         x, y = c*w+w/2, self.wh-h-r*h+h/2
-        s = pyglet.sprite.Sprite(img=img, x=x, y=y, batch=self.batch, group=grp, subpixel=SUBPIX)
+        s = pyglet.sprite.Sprite(img, x, y, batch=self.batch, group=grp, subpixel=SUBPIX)
         s.opacity = self.getOpacity(OPC, i, ni)
         if c==0 or r==0 or c==self.nc-1 or r==self.nr-1:
             self.dumpSprite('{:20} [{:3}] [{:3}] {:8.2f} {:8.2f} {:4} {:4}'.format(reason, r, c, x, y, img.anchor_x, img.anchor_y), s)
         return s
+
+    @staticmethod
+    def getMeshColor(i, j, mesh):
+        MIN, NOM, MAX = 0, 1, 2
+        MESH          = [(255, 0, 0), (0, 255, 0), (255, 255, 255)]
+        if   (i-j) % mesh[MAX]==0: return MESH[MAX]
+        elif (i-j) % mesh[NOM]==0: return MESH[NOM]
+        elif (i-j) % mesh[MIN]==0: return MESH[MIN]
 
     @staticmethod
     def getOpacity(opacity=255, i=0, ni=0):
@@ -175,18 +162,18 @@ class TestGui(pyglet.window.Window):
         for c in range(nc+1):
             x1, x2, y1, y2 = c*w, c*w, 0, nr*h
             self.clines[c].position = (x1, y1, x2, y2)
-            if dbg: print('c=[{:3}] x1={:6.1f} y1={:6.1f} x2={:6.1f} y2={:6.1f}'.format(c, x1, y1, x2, y2), file=DBG_FILE)
+            if dbg: print('c=[{:3}] x1={:7.2f} y1={:7.2f} x2={:7.2f} y2={:7.2f}'.format(c, x1, y1, x2, y2), file=DBG_FILE)
         for r in range(nr+1):
             x1, x2, y1, y2 = 0, nc*w, r*h, r*h
             self.rlines[r].position = (x1, y1, x2, y2)
-            if dbg: print('r=[{:3}] x1={:6.1f} y1={:6.1f} x2={:6.1f} y2={:6.1f}'.format(r, x1, y1, x2, y2), file=DBG_FILE)
+            if dbg: print('r=[{:3}] x1={:7.2f} y1={:7.2f} x2={:7.2f} y2={:7.2f}'.format(r, x1, y1, x2, y2), file=DBG_FILE)
         if self.useSciSprts:
             self.dumpSprite(None)
             for r in range(nr):
                 for c in range(nc):
                     x, y = c*w+w/2, wh-h-r*h+h/2
                     s = self.sciSprts[r][c]
-                    s.update(x=x, y=y, scale_x=ww/_ww, scale_y=wh/_wh)
+                    s.update(x, y, scale_x=ww/_ww, scale_y=wh/_wh)
                     s.rotation = 22.5 * c
                     if c == 0 or r == 0 or c == nc-1 or r == nr-1:
                         self.dumpSprite('{:20} [{:3}] [{:3}] {:8.2f} {:8.2f} {:4} {:4}'.format('on_resize(sciSprts)', r, c, x, y, s.image.anchor_x, s.image.anchor_y), s)
@@ -196,7 +183,7 @@ class TestGui(pyglet.window.Window):
                 for c in range(nc):
                     x, y = c*w+w/2, wh-h-r*h+h/2
                     s = self.imgSprts[r][c]
-                    s.update(x=x, y=y, scale_x=self.iss[(r+c)%len(self.iss)][0]*ww/_ww, scale_y=self.iss[(r+c)%len(self.iss)][0]*wh/_wh)
+                    s.update(x, y, scale_x=self.iss[(r+c)%len(self.iss)][0]*ww/_ww, scale_y=self.iss[(r+c)%len(self.iss)][0]*wh/_wh)
                     s.rotation = 22.5 * c
                     if c == 0 or r == 0 or c == nc-1 or r == nr-1:
                         self.dumpSprite('{:20} [{:3}] [{:3}] {:8.2f} {:8.2f} {:4} {:4}'.format('on_resize(imgSprts)', r, c, x, y, s.image.anchor_x, s.image.anchor_y), s)
