97f3ab7077f784b32ad71f6af56cfacd91a6bb81
johnwclemens <jwclemens@yahoo.com>
2022-07-30 17:47:36 -0500

    tabsB.py part 6 sync util.py

--- util.py
+++ util.py
@@ -12,7 +12,12 @@ M12             = { 10:'a', 11:'b' }
 INTERVALS       = { 0:'R', 1:'b2', 2:'2', 3:'m3', 4:'M3', 5:'4', 6:'b5', 7:'5', 8:'#5', 9:'6', 10:'b7', 11:'7' }
 INTERVAL_RANK   = { 'R':0, 'b2':1, '2':2, 'm3':3, 'M3':4, '4':5, 'b5':6, '5':7, '#5':8, '6':9, 'b7':10, '7':11 }
 NTONES          = len(INTERVALS)
-STFILT = ['log', 'dumpGeom', 'resetJ', 'dumpJs', 'dumpImap', 'dumpSmap', 'dumpCursorArrows', '<listcomp>', 'dumpLimap2', 'dumpTniksPfx', 'dumpTniksSfx']
+INIT             = '###   Init   ###' *     13
+QUIT_BGN         = '###   BGN Quit   ###' * 10
+QUIT             = '###   Quit   ###'     * 13
+QUIT_END         = '###   END Quit   ###' * 10
+#STFILT = ['log', 'dumpGeom', 'resetJ', 'dumpJs', 'dumpImap', 'dumpSmap', 'dumpCursorArrows', '<listcomp>', 'dumpLimap2', 'dumpTniksPfx', 'dumpTniksSfx']
+STFILT = ['log', 'dumpGeom', 'resetJ', 'dumpJs', 'dumpImap', 'dumpSmap', 'dumpCursorArrows', '<listcomp>', 'dumpLimap2', 'dumpTniksPfx', 'dumpTniksSfx', 'fmtXYWH', 'kbkInfo'] # , 'dumpView', 'dumpLbox', 'dumpRect']
 ########################################################################################################################################################################################################
 def getFilePath(baseName, basePath, fdir='files', fsfx='.txt', dbg=1):
     if dbg: slog(f'{baseName = } {basePath = }')
@@ -21,25 +26,25 @@ def getFilePath(baseName, basePath, fdir='files', fsfx='.txt', dbg=1):
     if dbg: slog(f'{fileName = } {filePath = }')
     return filePath
 ########################################################################################################################################################################################################
-def fmtl(lst, w=None, u='>', d1='[', d2=']', sep=' ', ll=0, z=''):
-    assert type(lst) in (list, tuple, set, frozenset)
+def fmtl(ls, w=None, u='>', d1='[', d2=']', sep=' ', ll=0, z=''):
+    lts = (list, tuple, set, frozenset)  ;  dts = (int, float, str)
+    assert type(ls) in (lts)
     if d1 == '': d2 = ''
     w = w if w else ''
-    t = ''   ;   s = f'<{len(lst)}' if ll else ''
-    for i, l in enumerate(lst):
-        if type(l) in (list, tuple, set):
-#            d0 = sep + d1 if not i else d1    ;    d3 = d2 + sep
-            if type(w) in (list, tuple, set):       t += fmtl(l, w[i], u, d1, d2, sep, ll, z)
-            else:                                   t += fmtl(l, w,    u, d1, d2, sep, ll, z)
+    t = ''   ;   s = f'<{len(ls)}' if ll else ''
+    for i, l in enumerate(ls):
+        if type(l) in (lts):
+#            d0 = sep + d1 if not i else d1  ;  d3 = d2 + sep
+            if type(w) in (lts):               t += fmtl(l, w[i], u, d1, d2, sep, ll, z)
+            else:                              t += fmtl(l, w,    u, d1, d2, sep, ll, z)
         else:
-            ss = sep if i < len(lst)-1 else ''
-            if   type(l) is type:                   l = str(l)
-            elif l is None:                         l = 'None'
-            if   type(w) in (list, tuple, set):     t += f'{l:{u}{w[i]}{z}}{ss}'
-            elif type(l) is int:                    t += f'{l:{u}{w   }{z}}{ss}'
-            elif type(l) is float:                  t += f'{l:{u}{w   }{z}}{ss}'
-            elif type(l) is str:                    t += f'{l:{u}{w   }{z}}{ss}'
-            else:                                   msg = f'ERROR l={l} is type {type(l)}'   ;   slog(msg)   ;   raise SystemExit(msg)
+            ss = sep if i < len(ls)-1 else ''
+            if   type(l) is type:              l =  str(l)
+            elif l is None:                    l =  'None'
+            if   type(w) in (lts):             t += f'{l:{u}{w[i]}{z}}{ss}'
+            elif type(l) in (dts):             t += f'{l:{u}{w   }{z}}{ss}'
+            else:                              t += f'{l}{ss}'
+#            else:                                   msg = f'ERROR l={l} is type {type(l)}'   ;   slog(msg)   ;   raise SystemExit(msg)
     return s + d1 + t + d2
 
 def fmtm(m, w=1, d0=':', d1='[', d2=']'):
@@ -48,6 +53,13 @@ def fmtm(m, w=1, d0=':', d1='[', d2=']'):
         if   type(v) in (list, tuple, set):         t += f'{k}{d0}'   ;   t += fmtl(v, w)
         elif type(v) in (int, str):                 t += f'{k:>{w}}{d0}{v:<{w}} '
     return d1 + t.rstrip() + d2
+
+def ordSfx(n):
+    m = n % 10
+    if   m == 1 and n != 11: return 'st'
+    elif m == 2 and n != 12: return 'nd'
+    elif m == 3 and n != 13: return 'rd'
+    else:                    return 'th'
 ########################################################################################################################################################################################################
 def stackDepth(sfs):
     global MAX_STACK_DEPTH, MAX_STACK_FRAME
@@ -112,13 +124,12 @@ def copyFile(src, trg, file=None):
     slog(f'### {cmd} ###', file=file, so=so)
     os.system(f'{cmd}')
 ########################################################################################################################################################################################################
-def parseCmdLine(file=None):
-    dbg = 1
+def parseCmdLine(file=None, dbg=1):
     options = dict()
     key = ''
     vals = []
     largs = len(sys.argv)
-    if dbg: slog(f'argv={fmtl(sys.argv)}', file=file)
+    if dbg: slog(f'argv={fmtl(sys.argv[1:])}', file=file)  ;  slog(f'{sys.argv[0]}', pfx=0, file=file)
     for j in range(1, largs):
         argv = sys.argv[j]
         if len(argv) > 2 and argv[0] == '-' and argv[1] == '-':
