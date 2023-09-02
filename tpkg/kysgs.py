ece575eea3dba4db6b43e3137d8850cb867a03cc
johnwclemens <jwclemens@yahoo.com>
2023-09-02 11:36:29 -0500

    rv kysgs p3 fmt, rm oc, dmpKSVHdr() dumpKSH()

--- tpkg/kysgs.py
+++ tpkg/kysgs.py
@@ -20,20 +20,19 @@ M, P    = -7, 7
 FD, PFX = -2, 0
 
 def init(f):
-    global KSD, FD   ;   FD = f
+    global FD   ;   FD = f
     slog('BGN', f=FD)
     dmpKSVHdr(csv=0,   t=FLAT)
     dmpKSVHdr(csv=1,   t=FLAT)
-    KSD = initKSD(KSD, t=FLAT)
-    KSD = initKSD(KSD, t=SHRP)
+    initKSD(KSD, t=FLAT)
+    initKSD(KSD, t=SHRP)
     dmpKSVHdr(csv=0,   t=SHRP)
     dmpKSVHdr(csv=1,   t=SHRP)
     dumpData( csv=0)
     dumpData( csv=1)
     slog('END', f=FD)
 
-def initKSD(ks, t):
-#    slog('BGN', f=FD)
+def initKSD(ksd, t):
     assert t==FLAT or t==SHRP and ist(t, int),  f'{t=} {type(t)=}'
     if     t==FLAT:  i = 0  ;  j = 6   ;  s = M
     else:            i = 0  ;  j = 10  ;  s = P
@@ -49,16 +48,12 @@ def initKSD(ks, t):
         else:                                              ms = list(ms2)  ;  iz = list(iz2)
         jz = Scales.majIs(i)    ;   im = [i, m] #  ;   ns = []
         ns = [ name(j, t, n2=0 if ak > 5 else 1) for j in jz ]
-        # for j in jz:
-        #     a = name(j, t=t, n2=0 if ak > 5 else 1)
-        #     ns.append(a)
-        ks[k]  =  [ im, iz, ms, jz, ns ]
+        ksd[k]  =  [ im, iz, ms, jz, ns ]
         slog(fmtKSK(k, csv=0), p=PFX, f=FD)
         slog(fmtKSK(k, csv=1), p=0,   f=3)
         i  =   nextIndex(i, s)
         j  =   nextIndex(j, s)
-#    slog('END', f=FD)
-    return ks
+    global KSD   ;   KSD = ksd
 ########################################################################################################################################################################################################
 def dumpData(csv=0):
     slog(f'BGN', f=FD)
@@ -73,43 +68,36 @@ def dumpKSV(csv=0):
     for k in keys:    slog(fmtKSK(k, csv), p=0 if csv else PFX, f=f)
     dmpKSVHdr(csv)
 
-def OLD__dmpKSVHdr(csv=0, t=0):
-    c, d, m, n, o, f = (Z, Z, Y, Z, Z, 3)  if csv else ('^20', '^13', W, W, W*2, FD)
-    k = 2*P+1 if t == 0 else M if t == FLAT else P if t == SHRP else 1
-    kso, fsi, ii, ino, kst = 'Key Sigature Ordered', 'F/S/N Indices', 'Ionian Indices', 'Ionian Note Ordering', f'Key Sig Table {signed(k)}'
-    hdrs = ['KS', 'Type', 'N', f'{n}I', f'{n}{kso:{c}}', f'{o}{fsi:{d}}', f'{o}{ii:{d}}', f'{n}{ino:{c}}', f'{n}{kst}'] # fixme csv vs text file
-    hdrs = m.join(hdrs)    ;    slog(hdrs, p=PFX, f=f)
-
 def dmpKSVHdr(csv=0, t=0):
-    c, d, y, f = (Z, Z, Y, 3)  if csv else ('^20', '^13', W, FD)   ;   n, m = W, W*2
+    y, fd = (Y, 3)  if csv else (W, FD)   ;   n, m = W, W*2
     k = 2*P+1 if t == 0 else M if t == FLAT else P if t == SHRP else 1
     kso, fsi, ii, ino, kst = 'Key Sigature Ordered', 'F/S/N Indices', 'Ionian Indices', 'Ionian Note Ordering', f'Key Sig Table {signed(k)}'
-    hdrs = ['KS', 'Type', 'N', f'{n}I', f'{n}{kso:{c}}', f'{m}{fsi:{d}}', f'{m}{ii:{d}}', f'{n}{ino:{c}}', f'{n}{kst}'] # fixme csv vs text file
-    hdrs = y.join(hdrs)    ;    slog(hdrs, p=PFX, f=f)
+    hdrs = ['KS', 'Type', 'N', f'{n}I', f'{n}{kso}', f'{m}{fsi}', f'{m}{ii}', f'{n}{ino}', f'{n}{kst}']
+    hdrs = y.join(hdrs)    ;    slog(hdrs, p=PFX, f=fd)
 ########################################################################################################################################################################################################
 def fmtKSK(k, csv=0):
-    n = Y if csv else W   ;   w, d = 2, '['     ;    ak = abs(k)
+    y = Y if csv else W   ;   w, d = 2, '['     ;    ak = abs(k)
     t   = FLAT if k < 0 else SHRP if k > 0 else NTRL    ;   ntype = TYPES[t]
     s   = signed(k)       ;  im = KSD[k][KIM]   ;    i = im[0]    ;    m = im[1]
     iz  = KSD[k][KIS]     ;  jz = KSD[k][KJS]   ;   ms = KSD[k][KMS]
     ns  = [ name(j, t, 0 if ak > 5 else 1) for j in jz ] #    0 4 5 b
     iz  = [ f'{i:x}' for i in iz ]
     jz  = [ f'{j:x}' for j in jz ]
-    _ = [s, ntype, f'{m:{w}}', f'{i:x}', fmtl(ms, w=w, d=d, s=n), fmtl(iz, d=d, s=n), fmtl(jz, d=d, s=n), fmtl(ns, w=w, d=d, s=n)] # fixme csv vs text file
-    return n.join(_)
+    _ = [s, ntype, f'{m:{w}}', f'{i:x}', fmtl(ms, w=w, d=d, s=y), fmtl(iz, d=d, s=y), fmtl(jz, d=d, s=y), fmtl(ns, w=w, d=d, s=y)]
+    return y.join(_)
 ########################################################################################################################################################################################################
 def dumpKSH(csv=0):
+    y = Z if csv else W*2   ;   d, m, n = (Z, Y, Y) if csv else ('[', W, W*2)
+    v, fd   = (f'{W}{Y}', 3)    if csv else (W*2, FD)   ;   p = 0 if csv else PFX
     f, k, s = [c for c in 'Flats  '], [c for c in 'N'], [c for c in '  Shrps']
-    y, fd   = (f'{W}{Y}', 3)    if csv else (W*2, FD)   ;   p = 0 if csv else PFX
-    f       = y.join(f)   ;   k = y.join(k)   ;   s = y.join(s)
-    d, m, n = (Z, Y, Y) if csv else ('[', W, W*2)   ;   w = 2
-    hdrs = [ f'{f} ', f'{k} ', f'{s}' ]         ;  hdrs = m.join(hdrs)   ;  _ =  Z if csv else W  ;  slog(f'{_}{hdrs}', p=p, f=fd)
-    keys = sorted(KSD.keys())  ;  w = f'<{w}' ;   x = f'{w}x'
+    f = v.join(f)   ;   k = v.join(k)   ;   s = v.join(s)
+    hdrs = [ f'{f} ', f'{k} ', f'{s}' ]       ;  hdrs = m.join(hdrs)   ;  _ =  Z if csv else W  ;  slog(f'{_}{hdrs}', p=p, f=fd)
+    keys = sorted(KSD.keys())  ;  w = f'<2' ;   x = f'{w}x'
     _  = utl.ns2signs(keys)
-    z = y.join(_)   ;   _ = Z if csv else W   ;   slog(f'{_}{z}', p=p, f=fd) # {y}
+    z = v.join(_)   ;   _ = Z if csv else W   ;   slog(f'{_}{z}', p=p, f=fd)
     slog(f'{fmtl(list(map(abs, keys)), w=w, d=d, s=m)}', p=p, f=fd)
     _  = [ KSD[k][KIM][KSK]    for k in keys ]  ;   slog(f'{fmtl(_, w=x, d=d, s=m)}', p=p, f=fd)
-    _  = [ KSD[k][KIM][KST]    for k in keys ]  ;   slog(f'{fmtl(_, w=w, d=d, s=m)}', p=p, f=fd)    ;   y = Z if csv else W*2
+    _  = [ KSD[k][KIM][KST]    for k in keys ]  ;   slog(f'{fmtl(_, w=w, d=d, s=m)}', p=p, f=fd)
     f  = [ KSD[M][KMS][f]      for f in range(len(KSD[M][KMS])-1, -1, -1) ]  ;  s = [ KSD[P][KMS][s]    for s in range(len(KSD[P][KMS])) ]
     fs = []  ;  fs.extend(f)   ;  fs.append(y)  ;   fs.extend(s)   ;   slog(f'{fmtl(fs, w=w, d=d, s=m)}', p=p, f=fd)
     f  = [ f'{f:x}' for f in reversed(KSD[M][KIS]) ]   ;   s = [ f'{s:x}' for s in KSD[P][KIS] ]
